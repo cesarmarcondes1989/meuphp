4c43abc7397af11ef4dceffb5e205414b81c00b7
cesarmarcondes1989 <contato@cesar-marcondes.com>
2025-06-26 13:06:48 -0300

    Update gerar_imagem.php

--- gerar_imagem.php
+++ gerar_imagem.php
@@ -3,6 +3,8 @@
 $imagemBase = 'ranking.png';
 $image = imagecreatefrompng($imagemBase);
 
+
+
 // Define cor e fonte
 $corTexto = imagecolorallocate($image, 101, 67, 33);
 $fonte = __DIR__ . '/fonte_usar.ttf'; // Certifique-se que arial.ttf está no mesmo diretório
@@ -17,7 +19,9 @@ $lineHeight = 30;
 
 // Escreve o texto linha por linha
 foreach (explode("\n", $texto) as $linha) {
-    imagettftext($image, 50, 0, $x, $y, $corTexto, $fonte, $linha);
+    //imagettftext($image, 50, 0, $x, $y, $corTexto, $fonte, $linha);
+    imagestring($image, 5, $x, $y, "Posição: 1", $corTexto);
+    imagestring($image, 5, $x, $y + 20, "Pontuação: 850", $corTexto);
     $y += $lineHeight;
 }
 
