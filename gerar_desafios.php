61a2904226f83108a9d0366712328559ffec79d5
cesarmarcondes1989 <contato@cesar-marcondes.com>
2025-06-28 20:20:32 -0300

    Update and rename gerar_imagens.php to gerar_desafios.php

--- gerar_desafios.php
+++ gerar_desafios.php
@@ -1,6 +1,6 @@
 <?php
 // Sorteia um número aleatório entre 1 e 20
-$numeroSorteado = rand(1, 20);
+$numeroSorteado = rand(1, 22);
 
 // Formata o número para dois dígitos (ex: 1 vira 01)
 $numeroFormatado = str_pad($numeroSorteado, 2, "0", STR_PAD_LEFT);
